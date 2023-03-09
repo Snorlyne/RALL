be5e2bdc9124aa4a7a2377ff1abf86e4035d3953
Snorlyne <lianaguirre345@gmail.com>
2023-03-09 08:53:17 -0500

    Sistema de clima agregado y boton responsive

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -23,7 +23,14 @@ export class DashboardPage implements OnInit {
   activo2 = false;
 
   riego_on_off: string = 'Apagado';
-  foco_on_off: string = 'Apagado';
+
+  rutaImg: string = './assets/images/Sun.png';
+
+  imgSoleado = './assets/images/Sun.png';
+  imgLluvia = './assets/images/llovizna.png';
+  clima_sol_lluvia: string = 'Soleado';
+  llovido = false;
+  activar_tras_llovido = false;
 
   constructor(
     private alertController: AlertController,
@@ -57,19 +64,15 @@ export class DashboardPage implements OnInit {
   }
 
   menuRiego() {
-    if (this.data.activar_riego == false) {
-      this.riegoAlert();
-    } else if (this.data.activar_riego == true) {
-      this.activo = false;
-      this.enviarDatos();
-    }
-  }
-  menuFoco() {
-    if (this.data2.activar_foco == false) {
-      this.focoAlert();
-    } else if (this.data2.activar_foco == true) {
-      this.activo2 = false; 
-      this.enviarDatos2();
+    if (this.activar_tras_llovido == false) {
+      if (this.data.activar_riego == false) {
+        this.riegoAlert();
+      } else if (this.data.activar_riego == true) {
+        this.activo = false;
+        this.enviarDatos();
+      }
+    } else if (this.activar_tras_llovido == true) {
+      this.notificacionLluvia();
     }
   }
 
@@ -91,45 +94,7 @@ export class DashboardPage implements OnInit {
     }
   }
 
-  enviarDatos2() {
-    if (this.data2.activar_foco == false) {
-      const ruta = '/Jardin/activar_foco';
-      const datos = true;
-      this.foco_on_off = 'Encendido';
-      this.dataService.activar_foco(ruta, datos);
-    } else if (this.data2.activar_foco == true) {
-      const ruta = '/Jardin/activar_foco';
-      const datos = false;
-      this.foco_on_off = 'Apagado';
-      this.dataService.activar_foco(ruta, datos);
-    }
-  }
-
   //Alerta de foco
-  async focoAlert() {
-    const alert = await this.alertController.create({
-      header: '¿Estas seguro?',
-      cssClass: 'custom-alert',
-      buttons: [
-        {
-          text: 'No',
-          cssClass: 'alert-button-cancel',
-        },
-        {
-          text: 'Si',
-          cssClass: 'alert-button-confirm',
-          handler: () => {
-            console.log('Acción confirmada');
-            // Aquí puedes llamar a la función que desees
-            this.activo2 = true;
-            this.enviarDatos2();
-          },
-        },
-      ],
-    });
-
-    await alert.present();
-  }
 
   ngOnInit(): void {
     this.dataService.getData().subscribe((data) => {
@@ -150,18 +115,23 @@ export class DashboardPage implements OnInit {
         this.notificacionRiego();
       }
     });
-    this.dataService.leerDatos('/Jardin/activar_foco').subscribe((data) => {
-      if (data == false) {
-        this.activo2 = false;
-        this.foco_on_off = 'Apagado';
-      } else if (data == true) {
-        this.activo2 = true;
-        this.foco_on_off = 'Encendido';
-        this.notificacionFoco();
+    //comparación e insertación de la imagen dinamica
+    this.dataService.leerDatos('/Jardin/lluvia').subscribe((data) => {
+      this.data3 = data;
+      if (this.data3 <= 30) {
+        this.rutaImg = this.imgSoleado;
+        this.clima_sol_lluvia = 'Soleado';
+        if(this.activar_tras_llovido == true){
+          this.llovido = false;
+          this.regar();
+        }
+      } else if (this.data3 > 30) {
+        this.rutaImg = this.imgLluvia;
+        this.clima_sol_lluvia = 'Está lloviendo!';
+        this.llovido = true;
+        this.activar_tras_llovido = true;
+        this.dejar();
       }
-    });
-    this.dataService.leerDatos('/Jardin/sensor_luz').subscribe((data3) => {
-      this.data3 = data3;
     });
   }
 
@@ -174,7 +144,7 @@ export class DashboardPage implements OnInit {
 
   //cronometro
   regar() {
-    if (this.riegoActivo == false) {
+    if (this.riegoActivo == false && this.llovido == false ) {
       setTimeout(() => {
         this.minutos = this.minutos + 1;
         if (this.minutos == 60) {
@@ -184,26 +154,16 @@ export class DashboardPage implements OnInit {
         this.horas2 = this.horas;
         this.minutos2 = this.minutos;
         this.regar();
+        if(this.minutos2 >= 4){
+          this.activar_tras_llovido = false;
+        }
       }, 3000);
     }
   }
- async notificacionFoco(){
+  async notificacionRiego() {
     const alert = await this.alertController.create({
-      header: 'Se ha activado la luz artificial',
-      cssClass: 'custom-alert',
-      buttons: [
-        {
-          text: 'Entendido',
-          cssClass: 'alert-button-confirm',
-        },  
-      ],
-    });
-  
-    await alert.present();
-  }
- async notificacionRiego(){
-    const alert = await this.alertController.create({
-      header: 'Se ha activado el sistema de riego',
+      header: 'Activado',
+      message: 'Se ha activado el sistema de riego',
       cssClass: 'custom-alert',
       buttons: [
         {
@@ -212,8 +172,23 @@ export class DashboardPage implements OnInit {
         },
       ],
     });
-  
+
+    await alert.present();
+  }
+
+  async notificacionLluvia() {
+    const alert = await this.alertController.create({
+      header: 'Cuidado!',
+      message: 'Debido a que se ha detectado lluvia, recomendamos no activar el sistema de riego durante las proximas 8hrs',
+      cssClass: 'custom-alert',
+      buttons: [
+        {
+          text: 'Entendido',
+          cssClass: 'alert-button-confirm',
+        },
+      ],
+    });
+
     await alert.present();
   }
 }
-
